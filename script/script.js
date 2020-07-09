efc9217511af23ae393e96c6a7c4908fd6d25a30
Erick Oliveira <57360338+Koliveir@users.noreply.github.com>
2020-07-09 20:28:10 -0300

    Envio dos arquivos

---
+++ script/script.js
@@ -0,0 +1,24 @@
+function dm() {
+    if (window.document.getElementById('switch-shadow').checked) {
+        window.document.body.style.backgroundColor='#3D405B'
+        window.document.getElementsByClassName('bloco')[0].style.backgroundColor='#F4F1DE'
+        window.document.getElementsByClassName('bloco')[0].style.color='#E07A5F'
+        window.document.getElementsByClassName('bloco')[1].style.backgroundColor='#F4F1DE'
+        window.document.getElementsByClassName('bloco')[1].style.color='#E07A5F'
+        window.document.getElementsByClassName('bloco')[2].style.backgroundColor='#F4F1DE'
+        window.document.getElementsByClassName('bloco')[2].style.color='#E07A5F'
+        window.document.getElementsByClassName('bloco')[3].style.backgroundColor='#F4F1DE'
+        window.document.getElementsByClassName('bloco')[3].style.color='#E07A5F'
+    } else {
+        window.document.body.style.backgroundColor='#f04f42'
+        window.document.getElementsByClassName('bloco')[0].style.backgroundColor='whitesmoke'
+        window.document.getElementsByClassName('bloco')[0].style.color='#f04f42'
+        window.document.getElementsByClassName('bloco')[1].style.backgroundColor='whitesmoke'
+        window.document.getElementsByClassName('bloco')[1].style.color='#f04f42'
+        window.document.getElementsByClassName('bloco')[2].style.backgroundColor='whitesmoke'
+        window.document.getElementsByClassName('bloco')[2].style.color='#f04f42'
+        window.document.getElementsByClassName('bloco')[3].style.backgroundColor='whitesmoke'
+        window.document.getElementsByClassName('bloco')[3].style.color='#f04f42'
+    }
+    
+}
