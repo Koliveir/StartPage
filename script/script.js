68228ca0444cbfc5289f43d3e49f4afddd10136d
Erick Oliveira <57360338+Koliveir@users.noreply.github.com>
2020-07-10 22:35:31 -0300

    Alteração de cores no darkmode e retirada de focus placeholder

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 function dm() {
     if (window.document.getElementById('switch-shadow').checked) {
         window.document.body.style.backgroundColor='#3D405B'
+        window.document.getElementById('search').style.borderBottom='3px solid #F4F1DE'
         window.document.getElementsByClassName('bloco')[0].style.backgroundColor='#F4F1DE'
         window.document.getElementsByClassName('bloco')[0].style.color='#E07A5F'
         window.document.getElementsByClassName('bloco')[1].style.backgroundColor='#F4F1DE'
