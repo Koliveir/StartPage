5d11892062f20106dd94c1bde5dd385383916f7b
Erick Oliveira <57360338+Koliveir@users.noreply.github.com>
2020-07-13 15:56:20 -0300

    Alteração do tamanho do checkbox switch e cores
    Comecei a alterar o esquema de cores do CSS para facilitar a mudança de cores futuramente.

--- script/script.js
+++ script/script.js
@@ -1,25 +1,25 @@
 function dm() {
-    if (window.document.getElementById('switch-shadow').checked) {
-        window.document.body.style.backgroundColor='#3D405B'
-        window.document.getElementById('search').style.borderBottom='3px solid #F4F1DE'
-        window.document.getElementsByClassName('bloco')[0].style.backgroundColor='#F4F1DE'
-        window.document.getElementsByClassName('bloco')[0].style.color='#E07A5F'
-        window.document.getElementsByClassName('bloco')[1].style.backgroundColor='#F4F1DE'
-        window.document.getElementsByClassName('bloco')[1].style.color='#E07A5F'
-        window.document.getElementsByClassName('bloco')[2].style.backgroundColor='#F4F1DE'
-        window.document.getElementsByClassName('bloco')[2].style.color='#E07A5F'
-        window.document.getElementsByClassName('bloco')[3].style.backgroundColor='#F4F1DE'
-        window.document.getElementsByClassName('bloco')[3].style.color='#E07A5F'
+    if (window.document.getElementById('switch_check').checked) {
+        window.document.body.style.backgroundColor = 'var(--background-color-2)'
+        window.document.getElementById('search').style.borderBottom = '3px solid #F4F1DE'
+        window.document.getElementsByClassName('bloco')[0].style.backgroundColor = '#F4F1DE'
+        window.document.getElementsByClassName('bloco')[0].style.color = '#E07A5F'
+        window.document.getElementsByClassName('bloco')[1].style.backgroundColor = '#F4F1DE'
+        window.document.getElementsByClassName('bloco')[1].style.color = '#E07A5F'
+        window.document.getElementsByClassName('bloco')[2].style.backgroundColor = '#F4F1DE'
+        window.document.getElementsByClassName('bloco')[2].style.color = '#E07A5F'
+        window.document.getElementsByClassName('bloco')[3].style.backgroundColor = '#F4F1DE'
+        window.document.getElementsByClassName('bloco')[3].style.color = '#E07A5F'
     } else {
-        window.document.body.style.backgroundColor='#f04f42'
-        window.document.getElementsByClassName('bloco')[0].style.backgroundColor='whitesmoke'
-        window.document.getElementsByClassName('bloco')[0].style.color='#f04f42'
-        window.document.getElementsByClassName('bloco')[1].style.backgroundColor='whitesmoke'
-        window.document.getElementsByClassName('bloco')[1].style.color='#f04f42'
-        window.document.getElementsByClassName('bloco')[2].style.backgroundColor='whitesmoke'
-        window.document.getElementsByClassName('bloco')[2].style.color='#f04f42'
-        window.document.getElementsByClassName('bloco')[3].style.backgroundColor='whitesmoke'
-        window.document.getElementsByClassName('bloco')[3].style.color='#f04f42'
+        window.document.body.style.backgroundColor = 'var(--background-color-1)'
+        window.document.getElementsByClassName('bloco')[0].style.backgroundColor = 'whitesmoke'
+        window.document.getElementsByClassName('bloco')[0].style.color = 'var(--background-color-1)'
+        window.document.getElementsByClassName('bloco')[1].style.backgroundColor = 'whitesmoke'
+        window.document.getElementsByClassName('bloco')[1].style.color = 'var(--background-color-1)'
+        window.document.getElementsByClassName('bloco')[2].style.backgroundColor = 'whitesmoke'
+        window.document.getElementsByClassName('bloco')[2].style.color = 'var(--background-color-1)'
+        window.document.getElementsByClassName('bloco')[3].style.backgroundColor = 'whitesmoke'
+        window.document.getElementsByClassName('bloco')[3].style.color = 'var(--background-color-1)'
     }
-    
+
 }
